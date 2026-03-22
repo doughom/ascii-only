cc9a57d675bc05c6f9640c3ac70e510da91d986d
Doug Hom <204439961+doughom@users.noreply.github.com>
2026-03-22 08:20:42 -0700

    refactor: remove unnecessary code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 exitCode=0
 
-if [[ -n "$GITHUB_TOKEN" ]]; then
-  git config "url.https://x-access-token:$GITHUB_TOKEN@github.com.insteadOf" "https://github.com"
-fi
-
-git fetch origin "$GITHUB_BASE_REF"
-
 while IFS= read -r -d "" file; do
   output=$(mktemp)
   if LC_ALL=C grep -nP "[^[:ascii:]]" "$file" > "$output"; then
@@ -16,6 +10,6 @@ while IFS= read -r -d "" file; do
   else
     echo "::notice file=$file::OK"
   fi
-done < <(git diff --name-only -z "origin/$GITHUB_BASE_REF..$GITHUB_SHA")
+done < <(git diff --name-only -z "origin/$GITHUB_BASE_REF")
 
 exit $exitCode
