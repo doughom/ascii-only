5dd9183e72f426fa54d96ba14a7b41c7ef8476c7
Doug Hom <204439961+doughom@users.noreply.github.com>
2026-03-22 08:39:47 -0700

    feat: job summary

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-exitCode=0
+
+failedFiles=0
+
+echo "" >> "$GITHUB_STEP_SUMMARY"
+echo "| File | Status |" >> "$GITHUB_STEP_SUMMARY"
+echo "| --- | --- |" >> "$GITHUB_STEP_SUMMARY"
 
 while IFS= read -r -d "" file; do
   output=$(mktemp)
   if LC_ALL=C grep -nP "[^[:ascii:]]" "$file" > "$output"; then
     sed -i -E "s/^([0-9])+/::error file=$file,line=\1:/g" "$output"
     cat "$output"
-    ((exitCode+=1))
+    ((failedFiles+=1))
+    echo "| $file | FAIL |" >> "$GITHUB_STEP_SUMMARY"
   else
-    echo "::notice file=$file::OK"
+    echo "| $file | PASS |" >> "$GITHUB_STEP_SUMMARY"
   fi
 done < <(git diff --name-only -z "origin/$GITHUB_BASE_REF")
 
-exit $exitCode
+exit $failedFiles
